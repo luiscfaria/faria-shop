5cff2457a92c5b122bb2ef0dda9b51700f019925
Luis Faria <luiscarlossfaria@gmail.com>
2022-05-23 13:17:02 -0300

    fixed issue on server.js

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,11 @@ app.get('/', (req, res) => {
     res.send('API is running ...')
 });
 
-app.use(notFound)
-app.use(errorHandler)
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
