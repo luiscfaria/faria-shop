62c3c7dbb98e3ccc535faa9e164742162091abd0
Luis Faria <luiscarlossfaria@gmail.com>
2022-05-26 19:55:58 -0300

    Finished authtorization in the backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,21 +1,22 @@
 import asyncHandler from "express-async-handler";
+import generateToken from '../utils/generateToken.js'
 import User from "../models/userModel.js";
 
 // Description: Auth user and get token
 // Route: POST /api/users/login
 // Access: Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if(user && (await user.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -24,4 +25,59 @@ const authUser = asyncHandler(async (req, res) => {
 
 });
 
-export { authUser };
+// Description: Register new user
+// Route: POST /api/users
+// Access: Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user =  await User.create({
+        name, 
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+});
+
+// Description: Get user profile
+// Route: GET /api/users/profile
+// Access: Private
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+});
+
+export { authUser, registerUser, getUserProfile };
